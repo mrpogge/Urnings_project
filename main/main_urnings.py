f5c495c3756b1bfaac5dcf7619a68ce2496a9fe0
mr.pogge <gergely.bence98@outlook.com>
2022-03-26 22:16:47 +0100

    adaptive urn_size algo1

--- main/main_urnings.py
+++ main/main_urnings.py
@@ -1,33 +1,32 @@
 import pandas as pd
 import numpy as np
 import matplotlib as plt
+from scipy import rand
 import scipy.stats as sp
 import statsmodels.api as sm
 from statsmodels.graphics import tsaplots
 
 class Player:
 #class default constructor
-    def __init__(self, user_id, score, urn_size, true_value, so_score = 5): 
+    def __init__(self, user_id, score, urn_size, true_value, multiple_urn = False): 
         if score > urn_size:
             raise ValueError("The score can't be higher then the urn size.")
-
-        #basic attributes
-        self.user_id = user_id
-        self.score = score
-        self.urn_size = urn_size
-        self.est = self.score/self.urn_size
-        self.true_value = true_value
-        self.sim_y = 8
-        self.sim_true_y = 8
-
-        #attributes of the second order tracker
-        self.so_score = np.array([so_score, so_score, so_score])
-        self.so_urn_size = self.so_score * 3
+        if multiple_urn == False:
+            #basic attributes
+            self.user_id = user_id
+            self.score = score
+            self.urn_size = urn_size
+            self.est = self.score/self.urn_size
+            self.true_value = true_value
+            self.sim_y = 8
+            self.sim_true_y = 8
+            
+            #creating a container
+            self.container = np.array([self.score])
+            self.estimate_container = np.array([self.est])
+            self.differential_container = np.array([0])
         
-        #creating a container
-        self.container = np.array([self.score])
-        self.differential_container = np.array([0])
-        self.so_container = self.so_score 
+
 
     def draw(self, true_score_logic = False):
 
@@ -39,14 +38,6 @@ class Player:
             sim_y = np.random.binomial(1, self.true_value)
             self.sim_true_y = sim_y
             return sim_y
-
-    def draw_so(self):
-        
-        #expected second order result
-        expected_so = np.random.multinomial(1, self.so_score/self.so_urn_size)
-
-        return expected_so
-
 
     def autocorrelation(self, lag, plots = False):
         
@@ -72,10 +63,32 @@ class Player:
             
 
 class Game_Type:
-    def __init__(self, adaptivity, alg_type, updating_type = "one_dim"):
+    def __init__(self, 
+                adaptivity,
+                alg_type, 
+                updating_type = "one_dim", 
+                paired_update = False, 
+                adaptive_urn = False, 
+                min_urn = None,
+                max_urn = None,
+                freq_change = None,
+                window = None,
+                bound = None):
+
         self.adaptivity = adaptivity
         self.alg_type = alg_type
         self.updating_type = updating_type
+        self.item_pair_update = paired_update
+        self.adaptive_urn = adaptive_urn
+        self.min_urn = min_urn
+        self.max_urn = max_urn
+        self.freq_change = freq_change
+        self.window = window
+        self.bound = bound
+
+        #container for updates
+        self.queue = []
+        self.queue_direction = []
     
     def draw_rule(self, player, item):
         
@@ -107,22 +120,8 @@ class Game_Type:
 
             #CHECK WHAT HAPPENS EXACTLY IN MARIAS'S PAPER
             #calculating expected value
-            player.score += result
-            player.est = player.score / (player.urn_size + 1)
-            item.score += 1 - result
-            item.est = item.score / (item.urn_size + 1)
-
-            if player.score > player.urn_size:
-                player.score = player.urn_size
-                
-            if player.score < 0:
-                player.score = 0
-                
-            if item.score > item.urn_size:
-                item.score = item.urn_size
-                
-            if item.score < 0:
-                item.score = 0
+            player.est = (player.score + result) / (player.urn_size + 1)
+            item.est = (item.score + 1 - result) / (item.urn_size + 1)
 
             while player.sim_y == item.sim_y:
                     player.draw()
@@ -132,9 +131,7 @@ class Game_Type:
             player.sim_y = item.sim_y = 8
             
             #returning to the original urn conig
-            player.score -= result
             player.est = player.score / player.urn_size
-            item.score -= 1 - result
             item.est = item.score / item.urn_size
 
         return result, expected_results
@@ -190,7 +187,64 @@ class Game_Type:
             adaptivity_corrector = proposed_item_prob/current_item_prob
 
         return adaptivity_corrector
-  
+    
+    def paired_update(self, item, item_previous):
+        
+        if self.item_pair_update == True:
+            direction = item.score - item_previous
+
+            if direction == 0:
+                return item
+            elif len(self.queue) == 0:
+                self.queue.append(item)
+                self.queue_direction.append(direction)
+
+                #redo the update made in the metropolis step
+                item.score -= direction
+
+                if item.score > item.urn_size:
+                    item.score = item.urn_size
+                
+                if item.score < 0:
+                    item.score = 0
+
+                item.est = item.score / item.urn_size
+
+            elif (0-direction) in self.queue_direction:
+            
+                idxs = np.array(np.where(np.array(self.queue_direction) == (0-direction))[0])
+                random_pair_idx = int(np.random.choice(idxs, 1))
+
+                #updating the chosen items
+                random_pair = self.queue[random_pair_idx]
+
+                random_pair.score -= direction
+
+                if random_pair.score > random_pair.urn_size:
+                    random_pair.score = random_pair.urn_size
+                
+                if random_pair.score < 0:
+                    random_pair.score = 0
+                    
+                random_pair.est = random_pair.score / random_pair.urn_size
+
+        
+    def adaptive_urn_change(self, player):
+        
+        if self.adaptive_urn == True:
+            if len(player.differential_container) >= self.window:
+                conv_stat = player.differential_container[-self.window:]
+
+                #check the stats
+                if np.sum(conv_stat) >= self.bound and player.urn_size > self.min_urn:
+                    player.urn_size = player.urn_size / 2
+                    player.score = int(np.round(player.score / 2))
+                    player.est = player.score / player.urn_size
+                elif len(player.differential_container) % self.freq_change == 0 and player.urn_size < self.max_urn:
+                    player.urn_size = player.urn_size * 2
+                    player.score =  player.score * 2
+                    player.est = player.score / player.urn_size
+
         
 class Urnings:
     def __init__(self, players, items, game_type):
@@ -198,6 +252,8 @@ class Urnings:
         self.players = players
         self.items = items
         self.game_type = game_type
+        self.queue = self.game_type.queue
+        self.queue_direction = self.game_type.queue_direction
     
     #One can define other adaptivity rules, I will add this to gametype later
 
@@ -217,7 +273,7 @@ class Urnings:
 
         return adaptive_matrix
 
-    def matchmaking(self):
+    def matchmaking(self, ret_adaptivity_matrix = False):
 
         if self.game_type.adaptivity == "n_adaptive":
             player_index = np.random.randint(0, len(self.players))
@@ -233,6 +289,10 @@ class Urnings:
 
             return self.players[player_index], self.items[int(item_index)]
 
+        elif self.game_type.adaptivity == "adaptive" and ret_adaptivity_matrix == True:
+            adaptive_matrix = self.adaptive_rule_normal()
+
+            return adaptive_matrix
 
     def urnings_game(self, player, item):
         if type(player) != Player:
@@ -243,11 +303,11 @@ class Urnings:
         
         result, expected_results = self.game_type.draw_rule(player, item)
 
-        player_prop, item_prop = self.game_type.updating_rule(player, item, result, expected_results)
+        player_proposal, item_proposal = self.game_type.updating_rule(player, item, result, expected_results)
 
         #adding the metropolis step if needed
-        metropolis_corrector = self.game_type.metropolis_correction(player, item, player_prop, item_prop)
-        adaptivity_corrector = self.game_type.adaptivity_correction(player, item, player_prop, item_prop)
+        metropolis_corrector = self.game_type.metropolis_correction(player, item, player_proposal, item_proposal)
+        adaptivity_corrector = self.game_type.adaptivity_correction(player, item, player_proposal, item_proposal)
 
         acceptance = min(1, metropolis_corrector * adaptivity_corrector)
         u = np.random.uniform()
@@ -256,50 +316,46 @@ class Urnings:
         item_prev = item.score
 
         if u < acceptance:
-            player.score = player_prop
-            item.score = item_prop
+            player.score = player_proposal
+            item.score = item_proposal
             player.est = player.score / player.urn_size
             item.est = item.score / item.urn_size
+
+        #paired update
+        self.game_type.paired_update(item, item_prev)
+
+        #adaptive urn_size
+        self.game_type.adaptive_urn_change(player)
 
         #appending new update to the container
         player.container = np.append(player.container, player.score)
         item.container = np.append(item.container, item.score)
 
+        player.estimate_container = np.append(player.estimate_container, player.est)
+        item.estimate_container = np.append(item.estimate_container, item.est)
+
         #calculating the difference
         player_diff = player.score - player_prev
         item_diff = item.score - item_prev
-
-        #unleashing the power of the second order tracker
-        if player_diff == 1:
-            result_so = np.array([1, 0, 0])
-        elif player_diff == 0:
-            result_so = np.array([0, 1, 0])
-        elif player_diff == -1:
-            result_so = np.array([0, 0, 1])
-
-        #drawing the expected value
-        expected_so = player.draw_so()
-
-        #updating so urns
-        player.so_score = player.so_score + result_so - expected_so
 
         #appending second order results
         player.differential_container = np.append(player.differential_container, player_diff)
         item.differential_container = np.append(item.differential_container, item_diff)
 
-        #appending second order probabilities
-        player.so_container = np.append(player.so_container, player.so_score)
-
-        #print("Match between ", player.user_id, " and", item.user_id)
 
     def play(self, n_games, test = False):
         for ng in range(n_games):
             if test == True:
                 for pl in range(len(self.players)):
-                    item_index = np.random.randint(0, len(self.items))
-                    current_item = self.items[item_index]
-                    current_player = self.players[pl]
-                    self.urnings_game(current_player, current_item)
+                    #ERROR
+                    if self.game_type.adaptivity == "adaptive":
+                        adaptive_matrix = self.matchmaking(ret_adaptivity_matrix=True)
+                        item_index = np.random.choice(np.arange(len(self.items)), 1, p = (adaptive_matrix[pl,:] / np.sum(adaptive_matrix[pl,:])))
+                    else:
+                        item_index = np.random.randint(0, len(self.items))
+                        current_item = self.items[item_index]
+                    self.urnings_game(self.players[pl], current_item)
+                
             else:
                 current_player, current_item = self.matchmaking()
                 self.urnings_game(current_player, current_item)
---
+++ main/simulation_results.py
@@ -0,0 +1,143 @@
+import numpy as np
+import main_urnings as mu
+
+
+#recreating parameter space
+
+#changing parameters
+true_values = [0.6, 0.7, 0.8, 0.9]
+player_urn_sizes = [6, 10, 14, 18, 50]
+
+#fixed parameters
+n_player = 5000
+n_items = 1000
+item_urn_sizes = 1000
+n_sim = 300
+
+parameter_space_limit = np.zeros((len(true_values) * len(player_urn_sizes), 2))
+
+counter = 0
+for tv in range(len(true_values)):
+    for pus in range(len(player_urn_sizes)):
+        parameter_space_limit[counter, :] = true_values[tv], player_urn_sizes[pus]
+        counter += 1
+
+#NONADAPTIVE URNINGS WITH NO CHANGE
+#loading results of the simulation without adaptivity and change
+urnings_results_limit = np.load("urnings_array_limit.npy")
+
+for pars in range(len(true_values) * len(player_urn_sizes)):
+    #subsetting for each parameter
+    urnings_matrix = urnings_results_limit[:,:,pars]
+
+    
+    #calculating mean, standard error and CI95 per iteration 
+    col_means = np.mean(urnings_matrix, axis = 0) / parameter_space_limit[pars, 1]
+    col_var = np.std(urnings_matrix, axis = 0) / np.sqrt(n_player)
+
+    col_lower = np.zeros(len(col_means))
+    col_upper = np.zeros(len(col_means))
+    coverage = 0
+    for i in range(len(col_means)):
+        confint = np.percentile(urnings_matrix[:,i] / parameter_space_limit[pars, 1], [5, 95])
+        col_lower[i] = confint[0]
+        col_upper[i] = confint[1]
+        #calculating coverage
+        if confint[0] < parameter_space_limit[pars, 0] < confint[1]:
+            coverage += 1
+
+    coverage = coverage / n_sim
+
+    ######################## MEAN SQUARED ERROR #########################################
+    MSE = np.sum((parameter_space_limit[pars, 0] - col_means) ** 2) / len(col_means)
+    #print(MSE)
+    #save this to the results
+
+    ######################## HITTING TIME #########################################
+
+    SE_true_value = np.sqrt(parameter_space_limit[pars, 1] * parameter_space_limit[pars, 0] * (1 - parameter_space_limit[pars, 0]) / n_player)
+
+    #print out the iteration when the mean was in this error radius
+    error_radius_logical = []
+    for clm in col_means:
+        error_radius_logical.append((parameter_space_limit[pars, 0] - SE_true_value) < clm < (parameter_space_limit[pars, 0] + SE_true_value))
+    
+    trues_vec = np.where(error_radius_logical)[0]
+    if len(trues_vec) > 0:
+        hitting_time = np.min(trues_vec)
+    else:
+        hitting_time = 999
+
+    print(hitting_time)
+
+    ######################## COVERAGE #########################################
+
+    nominal_coverage_sim = np.random.binomial(parameter_space_limit[pars, 1], parameter_space_limit[pars, 0], (n_player, n_sim)) / parameter_space_limit[pars, 1]
+    
+    nominal_coverage = 0
+    for i in range(n_sim):
+        confint_nc = np.percentile(nominal_coverage_sim[:,i], [5, 95])
+        if confint_nc[0] < parameter_space_limit[pars, 0] < confint_nc[1]:
+            nominal_coverage += 1
+    
+    nominal_coverage = nominal_coverage / n_sim
+
+#ADAPTIVE URNINGS WITH NO CHANGE
+urnings_results_limit = np.load("urnings_array_limit_adaptive.npy")
+for pars in range(len(true_values) * len(player_urn_sizes)):
+    #subsetting for each parameter
+    urnings_matrix = urnings_results_limit[:,:,pars]
+
+    
+    #calculating mean, standard error and CI95 per iteration 
+    col_means = np.mean(urnings_matrix, axis = 0) / parameter_space_limit[pars, 1]
+    col_var = np.std(urnings_matrix, axis = 0) / np.sqrt(n_player)
+
+    col_lower = np.zeros(len(col_means))
+    col_upper = np.zeros(len(col_means))
+    coverage = 0
+    for i in range(len(col_means)):
+        confint = np.percentile(urnings_matrix[:,i] / parameter_space_limit[pars, 1], [5, 95])
+        col_lower[i] = confint[0]
+        col_upper[i] = confint[1]
+        #calculating coverage
+        if confint[0] < parameter_space_limit[pars, 0] < confint[1]:
+            coverage += 1
+
+    coverage = coverage / n_sim
+
+    ######################## MEAN SQUARED ERROR #########################################
+    MSE = np.sum((parameter_space_limit[pars, 0] - col_means) ** 2) / len(col_means)
+    #print(MSE)
+    #save this to the results
+
+    ######################## HITTING TIME #########################################
+
+    SE_true_value = np.sqrt(parameter_space_limit[pars, 1] * parameter_space_limit[pars, 0] * (1 - parameter_space_limit[pars, 0]) / n_player)
+
+    #print out the iteration when the mean was in this error radius
+    error_radius_logical = []
+    for clm in col_means:
+        error_radius_logical.append((parameter_space_limit[pars, 0] - SE_true_value) < clm < (parameter_space_limit[pars, 0] + SE_true_value))
+    
+    trues_vec = np.where(error_radius_logical)[0]
+    if len(trues_vec) > 0:
+        hitting_time = np.min(trues_vec)
+    else:
+        hitting_time = 999
+
+    #print(hitting_time)
+
+    ######################## COVERAGE #########################################
+
+    nominal_coverage_sim = np.random.binomial(parameter_space_limit[pars, 1], parameter_space_limit[pars, 0], (n_player, n_sim)) / parameter_space_limit[pars, 1]
+    
+    nominal_coverage = 0
+    for i in range(n_sim):
+        confint_nc = np.percentile(nominal_coverage_sim[:,i], [5, 95])
+        if confint_nc[0] < parameter_space_limit[pars, 0] < confint_nc[1]:
+            nominal_coverage += 1
+    
+    nominal_coverage = nominal_coverage / n_sim
+
+
